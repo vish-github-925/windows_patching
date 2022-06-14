d97aa9f5e20d2dd642fb5034d814cc3b6f5294df
= <uppunuthula925@gmail.com>
2022-06-14 12:29:35 +0530

    added win-updates-python file

---
+++ win-updates.py
@@ -0,0 +1,28 @@
+import json
+object_started = False
+with open("win-updates-searched.txt") as file:
+    objects = []
+    for line in file.readlines():
+        if line.startswith("{"):
+            object_started = True
+            obj = ""
+        if object_started:
+            obj += line
+        if line.startswith("}"):
+            object_started = False
+            objects.append(obj)
+            obj = ""
+
+dict_objects = []           
+for i in range(len(objects)):
+    dict_objects.append(json.loads(objects[i]))
+
+updates_available = []
+for i in range(len(dict_objects)):
+    if(bool(dict_objects[i]["kb"])):
+    #    print(dict_objects[i]["kb"])
+       updates_available.extend(dict_objects[i]["kb"])
+    else:
+        continue
+
+print(updates_available)
